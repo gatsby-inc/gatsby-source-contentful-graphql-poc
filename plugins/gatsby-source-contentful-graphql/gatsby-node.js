999a4fd01dcb89abc74d8cd93b6741149166fb1e
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2020-10-01 12:36:38 -0500

    Pagination adapter (EUREKA, IT WORKS 🤯)

--- plugins/gatsby-source-contentful-graphql/gatsby-node.js
+++ plugins/gatsby-source-contentful-graphql/gatsby-node.js
@@ -10,6 +10,31 @@ const {
 } = require(`gatsby-graphql-source-toolkit`);
 require("dotenv").config();
 
+const PaginateContentful = {
+  name: "LimitOffsetContenful",
+  expectedVariableNames: [`limit`, `offset`],
+  start() {
+    return {
+      variables: { limit: 20, offset: 0 },
+      hasNextPage: true,
+    };
+  },
+  next(state, page) {
+    const limit = Number(state.variables.limit) || 100;
+    const offset = Number(state.variables.offset) + limit;
+    return {
+      variables: { limit, offset },
+      hasNextPage: page.items.length === limit,
+    };
+  },
+  concat(result, page) {
+    return result.items.concat(page);
+  },
+  getItems(pageOrResult) {
+    return pageOrResult.items;
+  },
+};
+
 async function createConfig(gatsbyApi) {
   const spaceId = process.env.SPACE_ID;
   const cdaToken = process.env.CDA_TOKEN;
@@ -26,23 +51,40 @@ async function createConfig(gatsbyApi) {
       queries: `
         query LIST_BLOG_POSTS($limit: Int, $offset: Int) {
           blogPostCollection(limit: $limit, skip: $offset) {
-            items { ..._BlogPostId_ ...BlogPostFragment }
+            items { ..._BlogPostId_ }
           }
         }
         fragment _BlogPostId_ on BlogPost {
           __typename
           sys { id }
         }
-        fragment BlogPostFragment on BlogPost {
-          remoteId: sys {
-            id
+      `,
+    },
+    {
+      remoteTypeName: `Asset`,
+      queries: `
+        query LIST_ASSETS($limit: Int, $offset: Int) {
+          assetCollection(limit: $limit, skip: $offset) {
+            items { ..._AssetId_ }
           }
-          title
-          slug
-          heroImage {
-            url
+        }
+        fragment _AssetId_ on Asset {
+          __typename
+          sys { id }
+        }
+      `,
+    },
+    {
+      remoteTypeName: `Person`,
+      queries: `
+        query LIST_PERSONS($limit: Int, $offset: Int) {
+          personCollection(limit: $limit, skip: $offset) {
+            items { ..._PersonId_ }
           }
-          body
+        }
+        fragment _PersonId_ on Person {
+          __typename
+          sys { id }
         }
       `,
     },
@@ -80,6 +122,7 @@ async function createConfig(gatsbyApi) {
     },
     gatsbyTypePrefix: `Contentful`,
     gatsbyNodeDefs: buildNodeDefinitions({ gatsbyNodeTypes, documents }),
+    paginationAdapters: [PaginateContentful],
   };
 }
 
